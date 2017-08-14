d75bb5f8270ff706204055b549dc365d414b1ee0
Sam <USAM9PR@PEROOT.COM>
2017-08-14 07:53:38 +0530

    14 august

--- apache/recipes/default.rb
+++ apache/recipes/default.rb
@@ -1,8 +1,47 @@
 #
-# Cookbook Name:: apache
+# Cookbook:: apache
 # Recipe:: default
 #
-# Copyright 2011, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+y=`whoami`
+
+node.override['port']=9090
+
+case node['platform']
+	
+	when /ceNt.*s/i,'redhat','fedaro'
+		pack="httpd"
+	
+	when "debien","ubuntu"
+		pack="apache2"
+	
+end
+
+package "#{pack}" do	
+	action :install
+	not_if "rpm -qa | grep -i #{pack}"
+end
+
+
+cookbook_file "/var/www/html/index.html" do
+	source "index.html"
+end
+
+
+template "/etc/httpd/conf/httpd.conf" do
+         source "httpd.conf.erb"
+	 variables({
+	 :owner => y
+	 })
+	 notifies :restart,"service[#{pack}]",:delayed
+end
+
+
+service "#{pack}" do
+	action :start 
+end
+
+include_recipe "apache::sam"
+
+	
